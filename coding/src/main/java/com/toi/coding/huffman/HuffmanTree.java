f727d7cfadba3c65803cf63e76bab851f4b6a483
damuko <greatdamuko@gmail.com>
2015-10-12 13:29:16 +0400

    Increased accuracy of Integer Probability.

--- coding/src/main/java/com/toi/coding/huffman/HuffmanTree.java
+++ coding/src/main/java/com/toi/coding/huffman/HuffmanTree.java
@@ -4,9 +4,9 @@ import com.sun.istack.internal.NotNull;
 
 public abstract class HuffmanTree implements Comparable<HuffmanTree> {
     //its using to cast float probability to int
-    public final static int HUNDRED_PERCENTS = 100;
-    public final float probability; // the probability of this tree
-    public HuffmanTree(float prob) { probability = prob; }
+    public final static int HUNDRED_PERCENTS = 1000;
+    public final int probability; // the probability of this tree
+    public HuffmanTree(int prob) { probability = prob; }
 
     // compares on the probability
     public int  compareTo(HuffmanTree tree) {
