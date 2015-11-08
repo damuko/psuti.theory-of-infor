fc421c23996faa99a434570e55fdcb4d6d7609df
damuko <greatdamuko@gmail.com>
2015-11-08 19:38:17 +0400

    Added unit tests for the getSymbols method.

--- generator/src/test/edu/psuti/toi/generator/ConfigurationUtilsTest.java
+++ generator/src/test/edu/psuti/toi/generator/ConfigurationUtilsTest.java
@@ -7,11 +7,11 @@ import org.testng.annotations.Test;
 public class ConfigurationUtilsTest {
 
     @Test
-    public void testMatrixValidationPositive3x3(){
-        float[][] testUtilMatrix1 = new float[][] {
-                new float[] {0.3f, 0.4f, 0.3f},
-                new float[] {0.5f, 0.3f, 0.2f},
-                new float[] {0.2f, 0.3f, 0.5f}};
+    public void testMatrixValidationPositive3x3() {
+        float[][] testUtilMatrix1 = new float[][]{
+                new float[]{0.3f, 0.4f, 0.3f},
+                new float[]{0.5f, 0.3f, 0.2f},
+                new float[]{0.2f, 0.3f, 0.5f}};
 
         boolean check = ConfigurationUtils.validateProbabilityMatrix(testUtilMatrix1);
         System.out.println("Test validation matrix - Positive: " + check);
@@ -20,12 +20,12 @@ public class ConfigurationUtilsTest {
     }
 
     @Test
-    public void testMatrixValidationAll01Negative(){
+    public void testMatrixValidationAll01Negative() {
 
-        float[][] testUtilMatrix2 = new float[][] {
-                new float[] {0.1f, 0.1f, 0.1f},
-                new float[] {0.1f, 0.1f, 0.1f},
-                new float[] {0.1f, 0.1f, 0.1f}};
+        float[][] testUtilMatrix2 = new float[][]{
+                new float[]{0.1f, 0.1f, 0.1f},
+                new float[]{0.1f, 0.1f, 0.1f},
+                new float[]{0.1f, 0.1f, 0.1f}};
 
         boolean check = ConfigurationUtils.validateProbabilityMatrix(testUtilMatrix2);
         System.out.println("Test validation matrix with all 0.1f: " + check);
@@ -33,7 +33,30 @@ public class ConfigurationUtilsTest {
         Assert.assertTrue(!check);
     }
 
+    @Test
+    public void testGetSymbolsWithNewLine() {
+        String strWithNewLine = "\n2,1,3";
+        char[] expected = {'2', '1', '3'};
+        char[] actual = ConfigurationUtils.getSymbols(strWithNewLine);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(
+            expectedExceptions = {IllegalArgumentException.class}
+    )
+    public void testGetSymbolsEmptyStr() {
+        String parsedStr = "";
+        ConfigurationUtils.getSymbols(parsedStr);
+    }
 
 
-//    TODO: add tests for getSymbolsFromString method
+    @Test(
+            expectedExceptions = {IllegalArgumentException.class}
+    )
+    public void testGetSymbolsFromNotSeparatedString() {
+        String parsedStr = "asdasd";
+        ConfigurationUtils.getSymbols(parsedStr);
+    }
+
 }
