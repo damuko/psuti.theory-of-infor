0b775880c36947f9e6d9f717a28b280e979d5c92
damuko <greatdamuko@gmail.com>
2015-11-20 01:01:47 +0400

    Changed catching for the readFile method. Unified behavior with the writeToFile method.

--- generator/src/main/java/edu/psuti/toi/generator/utils/IOUtils.java
+++ generator/src/main/java/edu/psuti/toi/generator/utils/IOUtils.java
@@ -1,6 +1,7 @@
 package edu.psuti.toi.generator.utils;
 
 import edu.psuti.toi.generator.Configuration;
+
 import static edu.psuti.toi.generator.utils.GeneratorUtils.*;
 
 import java.io.FileWriter;
@@ -11,19 +12,18 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class IOUtils {
-    private static final String INPUT_SIZE_EXCEPTION_TEXT ="Input size is incorrect!";
+    private static final String INPUT_SIZE_EXCEPTION_TEXT = "Input size is incorrect!";
     private static final String NUMBER_FORMAT_EXCEPTION_TEXT = "Use only numeric values to enter!";
     private static final String PROBABILITY_MATRIX_FORMAT_EXCEPTION_TEXT = "Probability matrix should be square. " +
             "Rows sum should be equals to 1";
 
-    public static String readFile(String path, Charset encoding){
+    public static String readFile(String path, Charset encoding) throws IOException {
         try {
             byte[] encoded = Files.readAllBytes(Paths.get(path));
-            return new String(encoded,encoding);
+            return new String(encoded, encoding);
         } catch (IOException io) {
-            System.err.println("An error occurred when attempting to read a file:" + path +
-            io.getMessage());
-            return "";
+            throw new IOException("An error occurred when attempting to read a file:" + path, io);
+
         }
     }
 
@@ -37,12 +37,12 @@ public class IOUtils {
         String[] lines = textToParse.split("\n");
         resultMatrix = new float[lines.length][];
 
-        for (int i = 0; i!= lines.length; i++) {
+        for (int i = 0; i != lines.length; i++) {
             String[] values = lines[i].split(",");
 
             //validation for vector
-            if(values.length==1) {
-                if (lines.length!= symbolsQuantity)
+            if (values.length == 1) {
+                if (lines.length != symbolsQuantity)
                     throw new IllegalArgumentException(INPUT_SIZE_EXCEPTION_TEXT);
             }
             //validation for square matrix
@@ -52,10 +52,10 @@ public class IOUtils {
             }
 
             resultMatrix[i] = new float[values.length];
-            for (int j = 0; j != values.length; j++){
+            for (int j = 0; j != values.length; j++) {
                 try {
-                    resultMatrix [i][j]= Float.parseFloat(values[j]);
-                } catch (NumberFormatException nfe){
+                    resultMatrix[i][j] = Float.parseFloat(values[j]);
+                } catch (NumberFormatException nfe) {
                     throw new NumberFormatException(NUMBER_FORMAT_EXCEPTION_TEXT);
                 }
             }
@@ -67,7 +67,7 @@ public class IOUtils {
 
     public static void printMatrix(float[][] resultProbMatrix, StringBuilder result) {
         if (resultProbMatrix != null) {
-            for (int i =0; i != resultProbMatrix.length; i++) {
+            for (int i = 0; i != resultProbMatrix.length; i++) {
                 for (int j = 0; j != resultProbMatrix[i].length; j++) {
                     result.append(Float.toString(resultProbMatrix[i][j])).append(',');
                 }
@@ -81,15 +81,15 @@ public class IOUtils {
             pw.println(text);
         } catch (IOException ex) {
             throw new IOException("An error occurred when attempting to save " +
-                    "generated sequence to file!");
+                    "generated sequence to file!", ex);
         }
     }
 
     public static void outputResults(
-             Configuration cfg
-            ,String generatedText
-            ,String outputFilePath
-            ,StringBuilder resultMatrixBuilder) throws IOException {
+            Configuration cfg
+            , String generatedText
+            , String outputFilePath
+            , StringBuilder resultMatrixBuilder) throws IOException {
 
         float[][] resProbabilityMatrix = calcResultProbability(generatedText, cfg);
 
